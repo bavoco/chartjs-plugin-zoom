e2965b4f1904a84fe415a1d2cca450af284df7eb
Bavo Coremans <bavoco@gmail.com>
2024-03-16 20:05:25 +0100

    more samples working

--- rollup.config.js
+++ rollup.config.js
@@ -99,4 +99,22 @@ module.exports = [
       indent: false
     },
   },
+  {
+    input: 'docs/scripts/utils.js',
+    plugins: [
+      commonjs({
+        include: 'node_modules/**',
+      }),
+      json(),
+      nodeResolve(),
+      terser({output: {comments: 'some'}})
+    ],
+    output: {
+      name,
+      file: `docs/public/utils.bundle.esm.js`,
+      banner,
+      format: 'esm',
+      indent: false
+    },
+  },
 ];
